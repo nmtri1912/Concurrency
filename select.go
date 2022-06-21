c5e718285319e4f78883b2d5e303fccc3275da8c
Tri Nguyen <nguyenminhtricpr@gmail.com>
2022-06-21 16:37:16 +0700

    select

---
+++ select.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func printSth(c chan int) {
+	for {
+		select {
+		case <-c:
+			fmt.Println("finish")
+			return
+		default:
+			fmt.Println("Loop")
+		}
+	}
+}
+
+func main() {
+	c := make(chan int)
+	go printSth(c)
+
+	time.Sleep(3 * time.Second)
+	c <- 5
+}
